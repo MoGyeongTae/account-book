2df984b26f4609a199e1a410616724a494f6b42b
gyeongtae-mo <gyeongtae.mo@3boon1.com>
2026-08-21 00:39:12 +0900

    remove: calendar page

--- app/calendar/page.tsx
+++
@@ -1,9 +0,0 @@
-import Calendar from "../_components/Calendar";
-
-export default function CalendarPage() {
-  return (
-    <div className="flex flex-1 bg-zinc-50 dark:bg-black">
-      <Calendar />
-    </div>
-  );
-}
